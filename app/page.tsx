99ce4fd9cc566136c3357fb420cef8c54b43c1e6
MarKsu <72389013+Marksu-u@users.noreply.github.com>
2026-06-23 16:46:52 +0200

    Composants GameCard réutilisable avec props

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import GameCard from "@/components/GameCard";
+
 export default function Home() {
   const titre = "Choisis ton jeu"
 
@@ -5,6 +7,9 @@ export default function Home() {
     <main>
       <h1>{titre}</h1>
       <p>Un jeu par jour</p>
+
+      {/**  Game Cards avec les différents */}
+      <GameCard label="Valorant"/>
     </main>
   );
 }
---
+++ components/GameCard.tsx
@@ -0,0 +1,6 @@
+
+export default function GameCard({ label }: { label: string}) {
+    return (
+        <div>{label}</div>
+    );
+}
