49558a19039b297c539cd30070b104cb34f11e82
Sara <saray@macbook-air2.home>
2026-07-08 00:08:58 +0200

    Titre+image centrée sur index

--- app/fifa-l1/page.tsx
+++ app/fifa-l1/page.tsx
@@ -3,19 +3,21 @@ import Image from 'next/image'
 export default function FifaPage() {
     return (
 
-       <div><h1>Ligue 1</h1>
+       <div><h1 className="text-3xl font-bold mb-6 text-center font-mono tracking-wide uppercase">Ligue 1</h1>
         <FifaImage/></div>
     )
 }
 
 export function FifaImage(){
     return (
+        <div className="flex justify-center w-full">
         <Image
             src="/fifa14.jpg"
-            width={400}
-            height={400}
+            width={300}
+            height={300}
             alt="Pochette de FIFA 14"
-        />
+            className="rounded-xl shadow-lg"
+        /></div>
     )
 }
 
