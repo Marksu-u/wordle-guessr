5f4f9f5e0df2a6bd1998caa7d9467a8aed64ac65
Sara <saray@macbook-air-de-sara.home>
2026-07-09 21:07:39 +0200

    Maj du 9/07

--- app/fifa-l1/page.tsx
+++ app/fifa-l1/page.tsx
@@ -1,15 +1,19 @@
 import Image from 'next/image'
 import WordleGame from '@/components/WordleGame';
+import wordsData from './data/ligue1/wordle.json';
 
 export default function FifaPage() {
     return (
-
-       <div>
-            <h1 className="text-3xl font-bold mb-4 text-center font-mono tracking-wide uppercase"> Wordle Game - Ligue 1</h1>
-            <p className='mb-6 text-center font-mono'>Retrouvez le nom d'un joueur du championnat français</p>
-            <FifaImage/>
-            <WordleGame/>
-        </div>
+        <main className="mx-auto min-h-screen flex-col justify-center">
+            <div>
+                <h1 className="p-6 max-w-5xl text-3xl font-bold mb-4 text-center font-mono tracking-wide uppercase">
+                    Wordle - Ligue 1
+                </h1>
+                <p className='mb-6 text-center font-mono'>Retrouvez le nom d'un joueur du championnat français</p>
+                <FifaImage/>
+                <WordleGame/>
+            </div>
+        </main>
     )
 }
 
