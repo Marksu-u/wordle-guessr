95ca83b62c30d0f583d503cad058716cb6d26f7e
Sara <saray@macbook-air-de-sara2.home>
2026-07-24 15:51:54 +0200

    Component du clavier

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -3,10 +3,11 @@
 //definition typescript poyr les props
 interface KeyboardProps {
     onKeyPress: (key: string) => void;
-    letterStatuses: { [key: string]: 'correct' | 'present' | 'absent' }; 
+    letterStatuses: { [key: string]: 'correct' | 'present' | 'absent' };
+    activeKey?: string | null;
 }
 
-export default function Keyboard({ onKeyPress, letterStatuses }: KeyboardProps) {
+export default function Keyboard({ onKeyPress, letterStatuses, activeKey }: KeyboardProps) {
     //matrice qui represente les touches de clavier
     const rows = [
         ['A', 'Z', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
@@ -21,6 +22,9 @@ return (
         <div key={rowIndex} className="flex gap-1 justify-center w-full">
           {row.map((key) => {
             const status = letterStatuses[key];
+
+            //Verification de pression de la touche
+            const isPressed = activeKey === key;
             
             // couleur de base des touches
             let bgClass = 'bg-zinc-700 hover:bg-zinc-600 cursor-pointer hover:scale-105 active:scale-95 transition-all text-white'; 
@@ -38,8 +42,11 @@ return (
               <button
                 key={key}
                 onClick={() => onKeyPress(key)}
-                className={`px-2 py-4 rounded font-bold text-sm min-w-[35px] 
-                  transition-all uppercase ${bgClass}`}>
+                className={`px-2 py-4 rounded font-bold text-sm min-w-[35px] uppercase
+                  transition-all duration-100 ease-out 
+                  ${bgClass}
+                  ${isPressed ? 'scale-90 brightness-150 shadow-inner ring-2 ring-white/50' :'scale-100'}
+                  `}>
                 {key}
               </button>
             );
