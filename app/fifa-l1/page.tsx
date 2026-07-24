a9b914af993be249120ffaa140881a4648148a61
Sara <saray@macbook-air-de-sara2.home>
2026-07-24 15:50:51 +0200

    Interface principale

--- app/fifa-l1/page.tsx
+++ app/fifa-l1/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import WordleGame from '@/components/WordleGame';
 import Link from 'next/link';
+import Score from '@/components/Score';
 
 export default function FifaPage() {
     return (
@@ -8,12 +9,17 @@ export default function FifaPage() {
                 <h1 className="p-6 text-3xl font-bold mb-1 text-center font-mono tracking-wide uppercase">
                     Wordle - Ligue 1
                 </h1>
+
                 <p className='mb-8 text-center font-mono'>Retrouvez le nom d'un joueur du championnat 
                     français</p>
 
                 <div className='flex flex-col md:flex-row gap-8 md:gap-16 justify-center items-center 
                 md:items-start w-full flex-grow'>
-                    <FifaImage/>
+                    <div className="flex flex-col items-center">
+                        <Score/>
+                        <FifaImage/>
+                    </div>
+                    
                     <div className='w-full max-w-xl'>
                         <WordleGame/>
                     </div>
@@ -24,15 +30,15 @@ export default function FifaPage() {
 
 export function FifaImage(){
     return (
-        <div className="flex justify-center w-full md:w-auto shrink-0 mt-30">
+        <div className="flex justify-center w-full md:w-auto shrink-0 mt-10 ">
             <Link
                 href="https://www.ligue1.com"
                 target="_blank"
                 rel="noopener noreferrer">
                 <Image
                     src="/ligue1.webp"
-                    width={300}
-                    height={300}
+                    width={250}
+                    height={250}
                     alt="Logo de Ligue 1"
                     title="Visiter le site officiel de la Ligue 1"
                     className="rounded-xl shadow-lg"
