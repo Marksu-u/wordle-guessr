7227fb0bf49d17a156a59bd7a588db4106b1b3c6
Sara <saray@macbook-air-de-sara2.home>
2026-07-28 01:58:56 +0200

    Logique du jeu et gestion des events

--- components/WordleGame.tsx
+++ components/WordleGame.tsx
@@ -5,6 +5,7 @@ import { useState, useEffect } from 'react';
 import WordleGrid from './WordleGrid';
 import Keyboard from './Keyboard';
 
+
 export default function WordleGame() {
     //OPTIONS DU JEU
     const [wordLength, setWordLength] = useState(5); //5 lettres par defaut
@@ -12,10 +13,8 @@ export default function WordleGame() {
 
     // ETATS DU JEU
     const [guesses, setGuesses] = useState<string[]>([]);
-
     //Stockage des couleurs
     const [evaluations, setEvaluations] = useState<('correct' | 'present' | 'absent')[][]>([]);
-
     //Mot en cours de saisie
     const [currentGuess, setCurrentGuess] = useState('');
 
@@ -23,7 +22,6 @@ export default function WordleGame() {
     const [gameStatus, setGameStatus] = useState<'playing' | 'won' | 'lost'>('playing');
 
     const [solution, setSolution] = useState<string>(''); //stockage du mot correct à afficher à la fin
-
     //Blocage double-clic
     const [isLoading, setIsLoading] = useState(false);
 
@@ -31,19 +29,63 @@ export default function WordleGame() {
     const [activeKey, setActiveKey] = useState<string | null>(null);
 
     const [toastMessage, setToastMessage] = useState<string | null>(null);
-
+    //Mouvement des cases quand le mot n'existe pas
+    const [isShaking, setIsShaking] = useState(false);
     // GESTION DU SCORE ET LOCALSTORAGE
     const [historiqueScores, setHistoriqueScores] = useState<Record<string, number>>({});
 
-    //Chargement historique
+    // TIROIR SAUVEGARDE DES PARTIES EN COURS
+    type Gamestate = {
+        guesses: string[];
+        evaluations: ('correct' | 'present' | 'absent')[][];
+        letterStatuses: { [key: string]: 'correct' | 'present' | 'absent' };
+        gameStatus: 'playing' | 'won' | 'lost';
+        solution: string;
+    };
+    //Dictionnaire des parties en cours
+    const [savedGames, setSavedGames] = useState<Record<number, Gamestate>>({});
+
+    //Sécurité de sauvegarde
+    const [isInitialized, setIsInitialized] = useState(false);
+
+    
+    //Chargement 
     useEffect(() => {
         const donneesSave = localStorage.getItem('ligue1-historique');
-        if (donneesSave) {
-            setHistoriqueScores(JSON.parse(donneesSave));
-        } else {
-            setHistoriqueScores({});
+        if (donneesSave) setHistoriqueScores(JSON.parse(donneesSave));
+        
+        const partiesSave = localStorage.getItem('ligue1-parties-en-cours');
+        if (partiesSave) {
+            const parsedSaves = JSON.parse(partiesSave);
+            setSavedGames(parsedSaves);
+
+            if (parsedSaves[5] && parsedSaves[5].guesses.length > 0) {
+                const s = parsedSaves[5];
+                setGuesses(s.guesses);
+                setEvaluations(s.evaluations);
+                setLetterStatuses(s.letterStatuses);
+                setGameStatus(s.gameStatus);
+                setSolution(s.solution);
+            }
         }
+        setIsInitialized(true);
     }, []);
+
+    //Sauvegarde automztique
+    useEffect(() => {
+        if (!isInitialized) return;
+        //Pas de sauvegarde de grille totalement vide
+        if (guesses.length === 0 && gameStatus === 'playing') return;
+
+        setSavedGames(prev => {
+            const newState = {
+                ...prev,
+                [wordLength]: { guesses, evaluations, letterStatuses, gameStatus, solution }
+            };
+            localStorage.setItem('ligue1-parties-en-cours', JSON.stringify(newState));
+            return newState;
+        })
+    }, [guesses, evaluations, letterStatuses, gameStatus, solution, wordLength, isInitialized]);
 
     //Sauvegarde du score
     const enregistrerScoreDuJour = (scoreObtenu: number) => {
@@ -77,7 +119,15 @@ export default function WordleGame() {
                 setCurrentGuess('');
                 setLetterStatuses({});
                 setGameStatus('playing');
-                setSolution(''); 
+                setSolution('');
+
+                //Efface l'ancienne sauvegarde pour cette taille
+                setSavedGames(prev => {
+                    const newState = { ...prev };
+                    delete newState[lengthToSet];
+                    localStorage.setItem('ligue1-parties-en-cours', JSON.stringify(newState));
+                    return newState;
+                });
             } 
         } catch (error) {
             console.error(error);
@@ -88,8 +138,22 @@ export default function WordleGame() {
 
     //Relance de la partie si user clique sur bouton de taille
     const handleLengthChange = (newLength: number) => {
-        if (isLoading) return;
-        startNewGame(newLength);
+        if (isLoading || newLength === wordLength) return;
+
+        const saved = savedGames[newLength];
+        //Si sauvegarde il y a, on la récupère
+        if (saved && saved.guesses.length > 0) {
+            setGuesses(saved.guesses);
+            setEvaluations(saved.evaluations);
+            setLetterStatuses(saved.letterStatuses);
+            setGameStatus(saved.gameStatus);
+            setSolution(saved.solution);
+            setCurrentGuess('');
+            setWordLength(newLength);
+        } else {
+            //Sinon lancement nouvelle partie
+            startNewGame(newLength);
+        }
     };
 
     //Gestion des touches
@@ -120,9 +184,11 @@ export default function WordleGame() {
 
                 if (!response.ok) {
                     //Affichage du message d'erreur
-                    setToastMessage("Mot introuvable dans la base de données");
+                    setToastMessage("Mot introuvable dans la liste");
                     // On le fait disparaître après 2 secondes
                     setTimeout(() => setToastMessage(null), 2000);
+                    setIsShaking(true);//Mouvement des cases
+                    setTimeout(() => setIsShaking(false), 400);
                     return;//le mot est bloqué
                 }
 
@@ -239,7 +305,7 @@ export default function WordleGame() {
     return (
         <div className='flex flex-col items-center w-full p-4'>
             {/* SELECTEUR TAILLE MOT */}
-            <div className='flex flex-col items-center mb-6 w-full'>
+            <div className='flex flex-col items-center mt-6 mb-12 w-full'>
                 <span className='text-xs font-mono uppercase tracking-wider text-zinc-400 mb-2'>
                     Longueur du mot
                 </span>
@@ -260,17 +326,25 @@ export default function WordleGame() {
                 </div>
             </div>
 
-            <WordleGrid
-                guesses={guesses}
-                currentGuess={currentGuess}
-                wordLength={wordLength}
-                evaluations={evaluations} // On passe directement les couleurs reçues du serveur !
-                maxAttempts={maxAttempts}//prop appliquée à la grille 
-            />
+        {/* Message d'erreur temporaire*/}
+        {toastMessage && (
+            <div className="absolute top-38 bg-zinc-900 text-white text-sm border border-zinc-700 px-4 py-3
+            rounded-lg shadow-2xl z-50 font-bold tracking-wider">
+                {toastMessage}
+            </div>
+        )}
+                <WordleGrid
+                    guesses={guesses}
+                    currentGuess={currentGuess}
+                    wordLength={wordLength}
+                    evaluations={evaluations} // On passe directement les couleurs reçues du serveur !
+                    maxAttempts={maxAttempts}//prop appliquée à la grille
+                    isShaking={isShaking}//prop du mouvement
+                />
 
              {/* AFFICHAGE DU MOT CORRECT EN CAS DE DÉFAITE */}
              { gameStatus === 'lost' && (
-                <div className='mb-4 p-3 bg-green-950/20 border border-green-800 rounded-xl text-center
+                <div className='mt-8 mb-4 p-3 bg-green-950/20 border border-green-800 rounded-xl text-center
                 w-full max-w-xs animate-fade-in'>
                     <p className='text-xs font-mono text-green-600 uppercase tracking-widest'>
                         La réponse correcte est :
@@ -283,7 +357,7 @@ export default function WordleGame() {
 
             {/*Affichage du bouton "Rejouer" en cas de partie terminée*/}
             {gameStatus !== 'playing' && (
-                <div className="flex flex-col items-center gap-3 my-4 ">
+                <div className="flex flex-col items-center mt-7 gap-3 my-4 ">
                     <p className="text-lg font-bold">
                         {gameStatus === 'won' ? 'Gagné ! ' : 'Dommage...'}
                     </p>
@@ -296,15 +370,17 @@ export default function WordleGame() {
                         {isLoading ? 'Génération...' : 'Rejouer'}
                     </button>
                 </div>
+            
             )}
 
-
             {/* LE CLAVIER VIRTUEL */}
-            <Keyboard 
-                onKeyPress={handleKeyPress} 
-                letterStatuses={letterStatuses} 
-                activeKey={activeKey}
-            />
+            <div className="mt-12 w-full">
+                <Keyboard 
+                    onKeyPress={handleKeyPress} 
+                    letterStatuses={letterStatuses} 
+                    activeKey={activeKey}
+                />
+            </div>
             
         </div>
     );
