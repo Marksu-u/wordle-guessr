9c4964b75408ceca104333cbbd026573b4a119c7
Sara <saray@macbook-air-de-sara2.home>
2026-07-27 17:49:44 +0200

    Component du clavier

--- components/Keyboard.tsx
+++ components/Keyboard.tsx
@@ -25,6 +25,10 @@ return (
 
             //Verification de pression de la touche
             const isPressed = activeKey === key;
+
+
+            //Bouton suuprimer remplacé par le logo
+            const isDelete = key === 'SUPPRIMER';
             
             // couleur de base des touches
             let bgClass = 'bg-zinc-700 hover:bg-zinc-600 cursor-pointer hover:scale-105 active:scale-95 transition-all text-white'; 
@@ -47,7 +51,15 @@ return (
                   ${bgClass}
                   ${isPressed ? 'scale-90 brightness-150 shadow-inner ring-2 ring-white/50' :'scale-100'}
                   `}>
-                {key}
+                {isDelete ? (
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor"
+                  className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 
+                    12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z"/>
+                  </svg>
+                ) : (
+                  key
+                )}
               </button>
             );
           })}
